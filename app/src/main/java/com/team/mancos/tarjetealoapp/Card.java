fd4b32e79c78acf8395c0ab70489f10eddc0f1a9
wilian misael caamal caamal <wiliancaamal31@gmail.com>
2018-06-05 01:22:00 -0500

    Modelo Tarjeta

---
+++ app/src/main/java/com/team/mancos/tarjetealoapp/Card.java
@@ -0,0 +1,36 @@
+package com.team.mancos.tarjetealoapp;
+
+public class Card {
+    //Variables
+
+    String id;
+    String description;
+    String concept;
+
+    //Setters and Getters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getConcept() {
+        return concept;
+    }
+
+    public void setConcept(String concept) {
+        this.concept = concept;
+    }
+
+
+}
